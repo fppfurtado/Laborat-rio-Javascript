f7eeb95ee91c8f039d4950d3c1cb32016916d474
Fernando Furtado <fernandofurtado92@hotmail.com>
2023-02-28 07:03:52 -0300

    adiciona comentarios com descrição do algoritmo

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+/*
+* O objetivo é criar uma função, ou conjunto de funções, que consiga
+* eliminar tabelas vazias do documento html.
+* Entende-se por 'tabelas vazias' toda tabela cujas linhas estão vazias,
+* ou seja, sem elementos table data (td).
+*
+* Proposta de Algoritmo
+*
+* 1. Recuperar uma lista de elementos <table> do documento
+* 2. Varrer a lista de elementos <table>, onde em cada iteração:
+* 2.1. Criar um contador da quantidade de linnhas (<tr>) da tabela
+* 2.2. Recuperar uma lista de elementos <tr> da tabela
+* 2.3. Varrer a lista de elementos <tr>, onde em cada iteração:
+* 2.3.1. Possui children?
+* 2.3.1.1. Sim => Recupera a lista e varre até ser falso
+* 2.3.1.2. Não => textContent é vazio? (entende-se por vazio espaço em branco, quebras de linhas, tabulações e afins)
+* 2.3.1.2.1. Sim => elimina o elemento
+*/
+
+
 removeElementosVazios = function(elemento) {
 
     regex = /(\n|\t|\s)/g;
